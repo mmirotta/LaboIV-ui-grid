c7749d984977c2529f4272f41df640ba875c6d65
mmirotta <m.mirotta@gmail.com>
2016-10-20 21:52:22 -0300

    Creado el marcador de mi posición. Nueva grilla de amigos

--- scripts/controllers/configuradoTP.js
+++ scripts/controllers/configuradoTP.js
@@ -1,8 +1,9 @@
 angular
   .module('app')
-  .controller('ConfiguradoTP', function($scope, data, i18nService, uiGridConstants, NgMap) {
-    $scope.googleMapsUrl="";
-
+  .controller('ConfiguradoTP', function($scope, data, i18nService, uiGridConstants, NgMap, $timeout) {
+    $scope.verMapa = false;
+    $scope.verAmigos = false;
+    $scope.DatosAmigos = [];
     $scope.titulo = "Configuracion Campos";
     // Objeto de configuracion de la grilla.
     $scope.gridOptions = {};
@@ -25,13 +26,13 @@ angular
         { field: 'id', name: '#', width: 45},
         { field: 'avatar', name: 'Avatar', cellTemplate: '<img src="{{COL_FIELD}}" style="width:30px"></img>', width: 45},
         { field: 'foto', name: 'Foto', cellTemplate: '<img src="{{COL_FIELD}}" style="width:20px"></img>', width: 45},
-        { field: 'nombre', name: 'nombre'
+        { field: 'nombre', name: 'nombre', width: 80
           ,enableFiltering: false
         },
-        { field: 'apellido', name: 'apellido'},
+        { field: 'apellido', name: 'apellido', width: 80},
         
-        { field: 'email', name: 'mail'},
-        { field: 'sexo', name: 'sexo'
+        { field: 'email', name: 'mail', width:210},
+        { field: 'sexo', name: 'sexo', width: 80
         // filtro de busqueda.
           ,filter: {
             // term: '1',
@@ -44,22 +45,54 @@ angular
           //filtro de los datos
           ,cellFilter: 'sexoTP'
         },
-        { field: 'fechaNacimiento', name: 'Fecha Nac'
+        { field: 'fechaNacimiento', name: 'Fecha Nac', width: 80
           ,type: 'date'
           ,cellFilter: "date: 'dd-MM-yyyy'"
         },
-        { field: 'sueldoPretendido', name: 'Sueldo'},
-        { field: 'amigos', name: 'amigos'},
-        { field: 'aplicaciones', name: 'aplicaciones'},
+        { field: 'sueldoPretendido', name: 'Sueldo', width: 80},
+        { name: 'Amigos', cellTemplate: '<button class="btn btn-primary" ng-click="grid.appScope.amigos(row.entity)">Amigos</button>', width: 80},
+        { name: 'Maps', cellTemplate: '<button class="btn btn-primary" ng-click="grid.appScope.localizar(row.entity)">Maps</button>', width: 70}
+      ];
+    };
+    $scope.gridAmigosOptions = {};
+    $scope.gridAmigosOptions.paginationPageSizes = [25, 50, 75];
+    // Configuracion de la paginacion
+    $scope.gridAmigosOptions.paginationPageSize = 25;
+    $scope.gridAmigosOptions.columnDefs = columnAmigosDefs();
+    // Activo la busqueda en todos los campos.
+    $scope.gridAmigosOptions.enableFiltering = true;
+    // Configuracion del idioma.
+    i18nService.setCurrentLang('es');
+
+    $scope.gridAmigosOptions.data = $scope.DatosAmigos;
+
+    function columnAmigosDefs () {
+      return [
+        { field: 'id', name: '#', width: 45},
+        { field: 'avatar', name: 'Avatar', cellTemplate: '<img src="{{COL_FIELD}}" style="width:30px"></img>', width: 45},
+        { field: 'foto', name: 'Foto', cellTemplate: '<img src="{{COL_FIELD}}" style="width:20px"></img>', width: 45},
+        { field: 'nombre', name: 'nombre'
+          ,enableFiltering: false
+        },
+        { field: 'apellido', name: 'apellido'},
         { name: 'Maps', cellTemplate: '<button class="btn btn-primary" ng-click="grid.appScope.localizar(row.entity)">Maps</button>', width: 45}
       ];
     };
 
     $scope.localizar = function(datos){
-          NgMap.getMap().then(function(map) {
-      console.log(map.getCenter());
-      console.log('markers', map.markers);
-      console.log('shapes', map.shapes);
-    });
+      $scope.verMapa = true;
+      NgMap.getMap().then(function(map) {
+        $scope.latitud = datos.latitud;
+        $scope.longitud = datos.longitud;
+      });
     }
+
+    $scope.amigos = function(datos){
+        $timeout(function() {
+          console.info(datos.amigos);
+          $scope.verAmigos=true;
+          $scope.DatosAmigos = datos.amigos;
+          console.info($scope.DatosAmigos);
+        });
+      }
   })
